9a6d46d528a060f21cc533d74e2e510d2d3b6507
Evan <enagy9000@gmail.com>
2026-05-11 14:22:32 -0400

    Module 3 completed baseline

--- Full Stack Development/travlr/app_server/controllers/travel.js
+++ Full Stack Development/travlr/app_server/controllers/travel.js
@@ -1,6 +1,11 @@
+var fs = require("fs");
+// wasnt working correctly so I used an absolute path instead.
+//var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+var trips = JSON.parse(fs.readFileSync('/home/mrbear/Desktop/GitHub Repository/SNHU/Full Stack Development/travlr/app_server/data/trips.json','utf8'));
+
 /* GET travel view*/
 const travel = (req,res) => {
-    res.render('travel',{ title: 'travlr getaways'})
+    res.render('travel',{ title: 'travlr getaways',trips})
 }
 
 module.exports = {
